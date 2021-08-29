7c8775eeff2afb3dabb9ee823b2693985b647158
jxstxn__ <Justinbaumann12112001@gmail.com>
2021-08-29 16:18:52 +0200

    started working on the Bloc

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:number_trivia_tdd/features/number_trivia/domain/entities/number_trivia.dart';
 
 part 'number_trivia_event.dart';
 part 'number_trivia_state.dart';
--- lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
@@ -1,8 +1,33 @@
 part of 'number_trivia_bloc.dart';
+
 abstract class NumberTriviaState extends Equatable {
   const NumberTriviaState();
 }
-class NumberTriviaInitial extends NumberTriviaState {
+
+class Empty extends NumberTriviaState {
   @override
   List<Object> get props => [];
+}
+
+class Loading extends NumberTriviaState {
+  @override
+  List<Object> get props => [];
+}
+
+class Loaded extends NumberTriviaState {
+  const Loaded({required this.trivia});
+
+  final NumberTrivia trivia;
+
+  @override
+  List<Object?> get props => [trivia];
+}
+
+class Error extends NumberTriviaState {
+  const Error({required this.message});
+
+  final String message;
+
+  @override
+  List<Object?> get props => [message];
 }
